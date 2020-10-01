d2a52f5c77192130f5a611e3d96b7f62712e91df
Quentin Marcuzzi <quentin.marcuzzi@ent.unilim.fr>
2020-10-01 15:18:25 +0200

    First Commit

---
+++ QuentinsLib/QuentinsLib.cpp
@@ -0,0 +1,55 @@
+// QuentinsLib.cpp : Définit les fonctions exportées de la DLL.
+//
+#include "pch.h"
+#include "QuentinsLib.h"
+
+/*
+* #######################################################
+* #                                                     #
+* # Fonction retournant le PGCD de 2 entiers non signés #
+* # Entrée : 2 nombres entiers non signés               #
+* # Sortie : Un entier non signé                        #
+* #                                                     #
+* #######################################################
+*/
+unsigned int PGCD(unsigned int a, unsigned int b) {
+	while (a != b) {
+		if (a > b) {
+			a -= b;
+		}
+		else {
+			b -= a;
+		}
+	}
+	return a;
+}
+
+/*
+* ############################################################################################
+* #                                                                                          #
+* # Fonction retournant un entier permettant de faciliter l'étape de saisie et vérifications #
+* # Entrée : booléen permettant d'indiquer le signe (true pour positif / false pour le reste #
+* #			 Chaîne de caractères contenant le message à afficher                            #
+* # Sortie : entier                                                                          #
+* #                                                                                          #
+* ############################################################################################
+*/
+int SaisirEntier(bool positif, string message) {
+	int nbr;
+	if (positif) {
+		do {
+			cout << message;
+			cin >> nbr;
+			if (nbr < 0) {
+				cout << "Les nombres négatifs ne sont pas acceptés !" << endl;
+				system("pause");
+				system("cls");
+			}
+		} while (nbr < 0);
+	}
+	else {
+		cout << message;
+		cin >> nbr;
+	}
+	return nbr;
+}
---
+++ QuentinsLib/QuentinsLib.h
@@ -0,0 +1,28 @@
+/*
+* Librairie personnelle alimentée au fur et à mesure des tp nécessitant des procédures et fonctions pour le module
+* d'introduction à l'algorithmique.
+* 
+* Auteur : Quentin Marcuzzi
+* Version : 1.0.0
+* Date de création : 01/10/2020
+* 
+*/
+
+// Le bloc ifdef suivant est la façon standard de créer des macros qui facilitent l'exportation
+// à partir d'une DLL plus simple. Tous les fichiers contenus dans cette DLL sont compilés avec le symbole QUENTINSLIB_EXPORTS
+// défini sur la ligne de commande. Ce symbole ne doit pas être défini pour un projet
+// qui utilise cette DLL. Ainsi, les autres projets dont les fichiers sources comprennent ce fichier considèrent les fonctions
+// QUENTINSLIB_API comme étant importées à partir d'une DLL, tandis que cette DLL considère les symboles
+// définis avec cette macro comme étant exportés.
+#ifdef QUENTINSLIB_EXPORTS
+#define QUENTINSLIB_API __declspec(dllexport)
+#else
+#define QUENTINSLIB_API __declspec(dllimport)
+#endif
+
+#include<iostream>
+using namespace std;
+
+extern "C++" unsigned int PGCD(unsigned int a, unsigned int b);
+
+extern "C++" int SaisirEntier(bool positif, string message);
